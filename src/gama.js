56296372ae0be35ae213b1e296a7eecd7bbaf6d1
Jan Brecka <jan.brecka@gmail.com>
2015-01-13 17:52:04 +0100

    use pipe

--- src/gama.js
+++ src/gama.js
@@ -481,10 +481,11 @@ gama.rotate = R.op(function(angle, matrix) {
  * @return {Matrix}
  */
 gama.rotateAboutPoint = R.curry(function(angle, point, matrix) {
-  matrix = gama.translate(point, matrix);
-  matrix = gama.rotate(angle, matrix);
-  matrix = gama.translate(gama.Point(-point.x, -point.y), matrix);
-  return matrix;
+  return R.pipe(
+    gama.translate(point),
+    gama.rotate(angle),
+    gama.translate(gama.negate(point))
+  )(matrix);
 });
 
 //----------------------------------------------
