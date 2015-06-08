f445f261b67a051b7feefff802e99d8cb0045f15
Jan Brecka <jan.brecka@gmail.com>
2015-06-08 14:08:10 +0200

    add tests for rotateMatrix function

--- test/rotateMatrix-test.js
+++ test/rotateMatrix-test.js
@@ -4,13 +4,39 @@ var E = require('./common').E;
 
 describe('gama.rotateMatrix', function() {
 
-  it('rotates matrix', function() {
+  it('rotates matrix (90°)', function() {
+    var result = gama.rotateMatrix(Math.PI * 0.5, gama.EmptyMatrix());
+    expect(result[0]).to.be.closeTo(0, E);
+    expect(result[1]).to.be.closeTo(-1, E);
+    expect(result[2]).to.be.closeTo(0, E);
+    expect(result[3]).to.be.closeTo(1, E);
+    expect(result[4]).to.be.closeTo(0, E);
+    expect(result[5]).to.be.closeTo(0, E);
+    expect(result[6]).to.be.closeTo(0, E);
+    expect(result[7]).to.be.closeTo(0, E);
+    expect(result[8]).to.be.closeTo(1, E);
+  });
+
+  it('rotates matrix (180°)', function() {
     var result = gama.rotateMatrix(Math.PI, gama.EmptyMatrix());
     expect(result[0]).to.be.closeTo(-1, E);
-    expect(result[1]).to.be.closeTo(-1.2246063538223773e-16, E);
+    expect(result[1]).to.be.closeTo(0, E);
     expect(result[2]).to.be.closeTo(0, E);
-    expect(result[3]).to.be.closeTo(-1.2246063538223773e-16, E);
+    expect(result[3]).to.be.closeTo(0, E);
     expect(result[4]).to.be.closeTo(-1, E);
+    expect(result[5]).to.be.closeTo(0, E);
+    expect(result[6]).to.be.closeTo(0, E);
+    expect(result[7]).to.be.closeTo(0, E);
+    expect(result[8]).to.be.closeTo(1, E);
+  });
+
+  it('rotates matrix (270°)', function() {
+    var result = gama.rotateMatrix(Math.PI * 1.5, gama.EmptyMatrix());
+    expect(result[0]).to.be.closeTo(0, E);
+    expect(result[1]).to.be.closeTo(1, E);
+    expect(result[2]).to.be.closeTo(0, E);
+    expect(result[3]).to.be.closeTo(-1, E);
+    expect(result[4]).to.be.closeTo(0, E);
     expect(result[5]).to.be.closeTo(0, E);
     expect(result[6]).to.be.closeTo(0, E);
     expect(result[7]).to.be.closeTo(0, E);
