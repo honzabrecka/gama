92f543a489fc5c9c8d855a267bd58da1dc41af9a
Jan Brecka <jan.brecka@gmail.com>
2017-08-18 15:32:57 +0200

    remove dist task and all related deps

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,6 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var uglify = require('gulp-uglify');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var header = require('gulp-header');
-
-var pkg = require('./package.json');
-var banner = [
-  '/**',
-  ' * <%= pkg.name %>',
-  ' *',
-  ' * <%= pkg.description %>',
-  ' *',
-  ' * @version v<%= pkg.version %>',
-  ' * @author <%= pkg.author %>',
-  ' * @license <%= pkg.license %>',
-  ' */',
-  ''
-].join('\n');
 
 gulp.task('test', function() {
   gulp
@@ -39,18 +20,6 @@ gulp.task('lint', function() {
     .src('./src/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
-});
-
-gulp.task('dist', function() {
-  browserify('./src/gama.js')
-    .require('./src/gama.js', {expose: 'gama'})
-    .require('ramda', {expose: 'ramda'})
-    .bundle()
-    .pipe(source('gama.min.js'))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('default', ['lint', 'test'], function() {
